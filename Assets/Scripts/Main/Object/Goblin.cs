a574cfda925424026c9ba8bcf83ba8de4e136570
Jun29785 <leejun9334@gmail.com>
2021-12-10 16:14:46 +0900

    [ADD] Goblin Move Animation

--- Assets/Scripts/Main/Object/Goblin.cs
+++ Assets/Scripts/Main/Object/Goblin.cs
@@ -6,10 +6,12 @@ using UnityEngine;
 // Abstract parent class inhibit
 public class Goblin : Actor
 {
+    bool isClicked;
     public override void Awake()
     {
         base.Awake();
-
+        // Move
+        StartCoroutine(Move());
     }
 
     public override void Start()
@@ -36,5 +38,22 @@ public class Goblin : Actor
         GetCoin = dict.GetCoin;
         Atk = dict.Atk;
         AtkDelay = dict.AtkDelay;
+    }
+
+    Vector2 RandomPosition(Vector2 vector)
+    {
+        return new Vector2(vector.x + (float)Random.Range(-10, 10) / 10f, vector.y + (float)Random.Range(-10, 10) / 10f);
+    }
+
+    IEnumerator Move()
+    {
+        yield return new WaitForSeconds(6f);
+        if (isClicked) { }
+        else
+        {
+            Vector2 vector = RandomPosition(this.gameObject.transform.position);
+            // Exception Out Range Check
+        }
+        StartCoroutine(Move());
     }
 }
